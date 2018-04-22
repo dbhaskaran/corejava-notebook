79a5924b85881bd13b83b9a8ed23cb895ff485c4
Deepak Bhaskaran <dbhaskaran@dbhaskaran-ltm4.internal.salesforce.com>
2018-04-22 14:16:13 -0400

    Added Stack impl using LinkedList

---
+++ src/main/java/org/dbhaskaran/ds/StackFromLL.java
@@ -0,0 +1,109 @@
+package org.dbhaskaran.ds;
+
+public class StackFromLL {
+
+	public static void main(String[] args) {
+		Stack s1 = new Stack();
+		System.out.println(s1.isEmpty());
+		s1.push(1);
+		s1.push(2);
+		s1.push(3);
+		s1.push(4);
+		s1.push(5);
+		System.out.println(s1.contains(3));
+		System.out.println(s1.peek());
+		s1.print();
+		s1.pop();
+		s1.print();
+
+	}
+
+}
+
+class Item {
+
+	int data;
+	Item next;
+
+	public Item(int data) {
+		this.data = data;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Item getNext() {
+		return next;
+	}
+
+	public void setNext(Item next) {
+		this.next = next;
+	}
+}
+
+class Stack {
+	Item top;
+
+	public Stack() {
+		top = new Item(0);
+		top.next = null;
+	}
+
+	// Push
+	public boolean push(int data) {
+		Item newItem = new Item(data);
+		newItem.setNext(top.getNext());
+		top.setNext(newItem);
+		return true;
+	}
+
+	// Pop
+	public int pop() {
+		Item curr = top.getNext();
+		if (curr == null)
+			return -1;
+		top.setNext(curr.getNext());
+		return curr.getData();
+	}
+
+	// Peek
+	public int peek() {
+		if (top.getNext() == null)
+			return -1;
+		return top.getNext().getData();
+	}
+
+	// IsEmpty
+	public boolean isEmpty() {
+		if (top.getNext() == null)
+			return true;
+		else
+			return false;
+	}
+
+	// Search
+	public boolean contains(int data) {
+		Item curr = top.getNext();
+		while (curr != null) {
+			if (curr.getData() == data)
+				return true;
+			curr = curr.getNext();
+		}
+		return false;
+	}
+
+	// Print
+	public void print() {
+		Item curr = top.getNext();
+		System.out.println("Stack: ");
+		while (curr != null) {
+			System.out.println(curr.getData());
+			curr = curr.getNext();
+		}
+	}
+}
